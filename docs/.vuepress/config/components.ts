214f02199ff277807e03250ec3e3237b9bb1c282
n-graymoon <huiyue.ning@71360.com>
2021-10-21 16:52:07 +0800

    no message

--- docs/.vuepress/config/components.ts
+++ docs/.vuepress/config/components.ts
@@ -1,7 +1,7 @@
 const {path} = require('@vuepress/utils')
 
 export const components={
-    MyDemo: path.resolve(__dirname, './components/my-demo.vue'),
-    Home: path.resolve(__dirname,'./components/home.vue'),
-    VueStillLearn : path.resolve(__dirname,'./components/vue3_still_learn/vue-still-learn.vue')
+    MyDemo: path.resolve(__dirname, '../components/my-demo.vue'),
+    Home: path.resolve(__dirname,'../components/home.vue'),
+    VueStillLearn : path.resolve(__dirname,'../components/vue3_still_learn/vue-still-learn.vue')
 };

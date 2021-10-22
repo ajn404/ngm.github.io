4867856e6425ded52a0bad9bb60fdba58cd95569
n-graymoon <huiyue.ning@71360.com>
2021-10-22 18:12:43 +0800

    什么鬼

--- docs/.vuepress/components/element-demo.vue
+++ docs/.vuepress/components/element-demo.vue
@@ -17,14 +17,27 @@
                 <el-table-column label="解释" prop="res"></el-table-column>
             </el-table>
         </div>
-        <el-calendar v-model="value"></el-calendar>
+        <el-calendar v-model="value" class="calendar"></el-calendar>
+        <el-tabs
+                type="card"
+                closable
+                v-model="tabsVal"
+                @tab-remove="removeTab"
+        >
+            <el-tab-pane
+                    v-for="item in tabs"
+                    :key="item.name"
+                    :label="item.title"
+                    :name="item.name"
+            >{{item.content}}</el-tab-pane>
+            <el-button type="primary" @click="addTab()">添加</el-button>
+
+        </el-tabs>
     </div>
 </template>
-
 <script lang="ts">
     import {defineComponent, ref} from 'vue'
     import {allegoricalList} from "./data/allegorical";
-
     export default defineComponent({
         setup() {
             const value = ref(new Date());
@@ -36,18 +49,72 @@
                     tableDataRight.push(value)
                 }
             });
-
             return {
+                //日历
                 value,
+                //表格
                 tableDataLeft: tableDataLeft,
-                tableDataRight: tableDataRight
+                tableDataRight: tableDataRight,
+                //
+                tabsVal:'',
+                tabs:[
+                    {
+                        title:'1',
+                        name:'1',
+                        content:'1'
+                    },
+                    {
+                        title:'2',
+                        name:'2',
+                        content:'2'
+                    }
+                ],
+                tabIndex:2
             }
+        },
+        methods: {
+            removeTab(targetName) {
+                const tabs = this.tabs;
+                let activeName = this.tabsVal
+                if (activeName === targetName) {
+                    tabs.forEach((tab, index) => {
+                        if (tab.name === targetName) {
+                            const nextTab = tabs[index + 1] || tabs[index - 1]
+                            if (nextTab) {
+                                activeName = nextTab.name
+                            }
+                        }
+                    })
+                }
+                this.tabsVal = activeName
+                this.tabs = tabs.filter((tab) => tab.name !== targetName)
+            },
+            addTab() {
+                const newTabName = `${++this.tabIndex}`
+                this.tabs.push({
+                    title: 'New Tab',
+                    name: newTabName,
+                    content: 'New Tab content',
+                })
+                this.tabsVal = newTabName
+            },
         }
     })
 </script>
 
-<style scoped>
+<style scoped lang="scss">
     .element-demo {
         margin-top: 20px;
     }
+    .calendar{
+        width: 100%;
+        ::v-deep(table){
+            display: table;
+        }
+        ::v-deep(.el-button-group){
+            display: flex;
+            gap: 10px;
+        }
+    }
+
 </style>
--- docs/menu.md
+++ docs/menu.md
@@ -1,21 +1,4 @@
-### 98年前端小生技术博客
-
-脑子不好，争取靠勤奋来补
-
-# 总目录
-
-```diff
-+ README.md						首页
-+ vue_still_learn
-+	README.md 					继续学习vue		
-+   webpack.md					webpack学习
-+	/js_skill					
-+		README.md				js技巧	
-+ vuepress_config
-+ README.md						博客配置
-+ plugins.md					vuepress插件
-```
-
+### element-plus
 <ClientOnly>
 <element-demo></element-demo>
 </ClientOnly>

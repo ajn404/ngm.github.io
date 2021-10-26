3ffc729f3b3e02c329c38c2da7a39a4d7bc74174
n-graymoon <huiyue.ning@71360.com>
2021-10-26 18:28:25 +0800

    no message

--- docs/.vuepress/components/responsiveImages/index.vue
+++ docs/.vuepress/components/responsiveImages/index.vue
@@ -1,17 +1,26 @@
 <template>
-    <div class="flex-box">
+    <div class="resIma">
+        <div class="flex-box">
 
-        <div v-for="item in fakeData" class="fake-images" :key="item.itemCity">
-            <el-image :src="item.itemUrl" style="" class="image-item">
-                <template #error>
-                    <div class="image-slot">
-                        <i class="el-icon-picture-outline"></i>
-                    </div>
-                </template>
-            </el-image>
+            <div v-for="item in fakeData.slice(0,21)" class="fake-images" :key="item.itemCity">
+                <el-image :src="item.itemUrl"  class="image-item">
+                    <template #error>
+                        <div class="image-slot">
+                            <i class="el-icon-picture-outline"></i>
+                        </div>
+                    </template>
+                </el-image>
+            </div>
         </div>
 
+        <h4>
+            flex布局
+        </h4>
+       <div class="flex-item1">
+           <span>{{fakeData[0].itemGit}}</span>
+       </div>
     </div>
+
 </template>
 
 <script lang="ts">
@@ -24,13 +33,15 @@
         },
         data() {
             let fakeData = [];
-            for (let i = 0; i < 21; i++) {
-                let {itemCity, itemUrl} = {
+            for (let i = 0; i < 100; i++) {
+                let {itemCity, itemUrl,itemGit} = {
                     itemCity:faker.address.city(),
-                    itemUrl:faker.image.image()
+                    itemUrl:faker.image.image(),
+                    itemGit:faker.git.branch()
                 }
-                fakeData.push({itemCity, itemUrl})
+                fakeData.push({itemCity, itemUrl,itemGit})
             }
+            // console.log(fakeData)
 
             return {
                 fakeData
@@ -40,18 +51,6 @@
 </script>
 
 <style scoped lang="scss">
-.flex-box{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    gap: 20px;
-    .fake-images{
-        flex: 0 0 20%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+    @import "../common/styles/responsiveImages/index";
 
-
-}
 </style>

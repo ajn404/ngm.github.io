9be38534ab14edeb522f4135c6c6a267f4292a14
ajn404 <huiyuening00@gmail.com>
2021-10-31 23:04:57 +0800

    no message

--- docs/.vuepress/components/home.vue
+++ docs/.vuepress/components/home.vue
@@ -1,98 +1,127 @@
 <template>
-    <div class="box" ref="box">
-        <div class="swiper-container">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <img src="./common/data/picture.jpg" alt="">
-                </div>
-                <div class="swiper-slide">
-                    <img src="./common/data/picture.jpg" alt="">
-                </div>
-                <div class="swiper-slide">
-                    <img src="./common/data/picture.jpg" alt="">
-                </div>
-            </div>
+  <div class="box" ref="box">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="https://picsum.photos/1920/480" alt="" />
         </div>
-        <span class="element"></span>
-        <div class="calendar">
-            ajn404
+        <div class="swiper-slide">
+          <img src="https://picsum.photos/1919/480" alt="" />
         </div>
-        <!-- <css-transition-and-animation></css-transition-and-animation> -->
-        <div class="box-box">{{text}}</div>
-        <md-editor class="editor" v-model="text"/>
+        <div class="swiper-slide">
+          <img src="https://picsum.photos/1922/480" alt="" />
+        </div>
+      </div>
     </div>
+    <span class="element"></span>
+    <div class="calendar">ajn404</div>
+    <!-- <css-transition-and-animation></css-transition-and-animation> -->
+    <div class="box-box">{{ text }}</div>
+    <md-editor class="editor" v-model="text" />
+  </div>
 </template>
 <script>
-    import Swiper from 'swiper'
-    import * as Typed from 'typed.js'
-    import GitHubCalendar from 'github-calendar'
-    // import cssTransitionAndAnimation from "./home/cssTransitionAndAnimation";
-    import MdEditor from 'md-editor-v3';
-    import 'github-calendar/dist/github-calendar-responsive.css'
-    import 'md-editor-v3/lib/style.css';
-    import 'swiper/css'
+import Swiper from "swiper";
+import * as Typed from "typed.js";
+import GitHubCalendar from "github-calendar";
+// import cssTransitionAndAnimation from "./home/cssTransitionAndAnimation";
+import MdEditor from "md-editor-v3";
+import "github-calendar/dist/github-calendar-responsive.css";
+import "md-editor-v3/lib/style.css";
+import "swiper/css";
+import "./common/styles/index.scss";
 
-    export default {
-        data() {
-            return {
-                text: 'hello editor'
-            }
+export default {
+  data() {
+    return {
+      text: "hello editor",
+    };
+  },
+  mounted() {
+    //轮播
+    this.initSwiper();
+    //自动打字
+    this.initType();
+    //github提交日历
+    this.initCalender();
+  },
+  methods: {
+    initSwiper() {
+      new Swiper(".swiper-container", {
+        autoplay: {
+          //自动切换
+          delay: 1000, //时延
+          disableOnInteraction: false,
         },
-        mounted() {
-            //轮播
-            this.initSwiper()
-            //自动打字
-            this.initType();
-            //github提交日历
-            this.initCalender();
-        },
-        methods: {
-            initSwiper() {
-                new Swiper('.swiper-container', {
-                    autoplay: true,
-                    effect: 'fade',//渐变切换
-                })
-            },
+        effect: "fade", //渐变切换
+        loop: true,
+        lazy: true,
+      });
+    },
 
-            initType() {
-                let typedString = ["大梦一场的", "董二千先生",
-                    "推开窗户", " 举起望远镜",
-                    "推开窗户", " 举起望远镜",
-                    "眼底映出 ", "一阵浓烟",
-                    "前已无通路 ", "后不见归途",
-                    "敌视现实", " 虚构远方",
-                    "东张西望 ", "一无所长",
-                    "四体不勤", " 五谷不分",
-                    "文不能测字", " 武不能防身",
-                    "喜欢养狗 ", "不爱洗头",
-                    "不事劳作 ", "一无所获",
-                    "厌恶争执", " 不善言说",
-                    "终于沦为", "沉默的帮凶",
-                    "借酒消愁 ", "不太能喝",
-                    "蛊惑他人 ", "麻醉内心",
-                    "浇上汽油 ", "舒展眉头",
-                    "纵火的青年 ", "迫近的时间",
-                    "大梦一场的", "董二千先生",
-                    "推开窗户 ", "举起望远镜",
-                    "眼底映出 ", "一阵浓烟",
-                    "前已无通路 ", "后不见归途"]
-                let homeTypedFont = new Typed('.element', {
-                    strings: typedString,
-                    typeSpeed: 500, //打字速度
-                    backSpeed: 10,//回退速度
-                });
-                return homeTypedFont
-            },
-            initCalender() {
-                GitHubCalendar(".calendar", "ajn404", {responsive: true});
-            }
-        },
-        components: {
-            //轨道环绕
-            // cssTransitionAndAnimation,
-            MdEditor
-        }
-    }</script>
+    initType() {
+      let typedString = [
+        "大梦一场的",
+        "董二千先生",
+        "推开窗户",
+        " 举起望远镜",
+        "推开窗户",
+        " 举起望远镜",
+        "眼底映出 ",
+        "一阵浓烟",
+        "前已无通路 ",
+        "后不见归途",
+        "敌视现实",
+        " 虚构远方",
+        "东张西望 ",
+        "一无所长",
+        "四体不勤",
+        " 五谷不分",
+        "文不能测字",
+        " 武不能防身",
+        "喜欢养狗 ",
+        "不爱洗头",
+        "不事劳作 ",
+        "一无所获",
+        "厌恶争执",
+        " 不善言说",
+        "终于沦为",
+        "沉默的帮凶",
+        "借酒消愁 ",
+        "不太能喝",
+        "蛊惑他人 ",
+        "麻醉内心",
+        "浇上汽油 ",
+        "舒展眉头",
+        "纵火的青年 ",
+        "迫近的时间",
+        "大梦一场的",
+        "董二千先生",
+        "推开窗户 ",
+        "举起望远镜",
+        "眼底映出 ",
+        "一阵浓烟",
+        "前已无通路 ",
+        "后不见归途",
+      ];
+      let homeTypedFont = new Typed(".element", {
+        strings: typedString,
+        typeSpeed: 500, //打字速度
+        backSpeed: 10, //回退速度
+      });
+      return homeTypedFont;
+    },
+    initCalender() {
+      GitHubCalendar(".calendar", "ajn404", { responsive: true });
+    },
+  },
+  components: {
+    //轨道环绕
+    // cssTransitionAndAnimation,
+    MdEditor,
+  },
+};
+</script>
 <style scoped>
-    @import "./common/styles/home.scss";
+@import "./common/styles/home.scss";
 </style>
--- docs/README.md
+++ docs/README.md
@@ -2,6 +2,7 @@
 pageClass: css-transition-and-animation-box
 sidebar: false
 sidebarDepth: 0
+home: true
 ---
 <ClientOnly>
 <Home></Home>

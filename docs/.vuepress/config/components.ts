81e88e808f6050d71b5e7a95bce560b5351cf9a0
n-graymoon <huiyue.ning@71360.com>
2021-11-02 16:32:26 +0800

    no message

--- docs/.vuepress/config/components.ts
+++ docs/.vuepress/config/components.ts
@@ -1,14 +1,14 @@
 const {path} = require('@vuepress/utils')
 
-export const components={
+export const components = {
     MyDemo: path.resolve(__dirname, '../components/draft/my-demo'),
-    Home: path.resolve(__dirname,'../components/home'),
-    VueStillLearn : path.resolve(__dirname,'../components/vue3_still_learn/vue-still-learn'),
-    NestedLines : path.resolve(__dirname,'../components/vue3_still_learn/nestedLines'),
-    ElementDemo : path.resolve(__dirname,'../components/element-demo'),
-    PixiDemo: path.resolve(__dirname,'../components/game/pixi-demo'),
-    ResponsiveImages:path.resolve(__dirname,'../components/responsiveImages/index'),
-    WaterfallsFlow:path.resolve(__dirname,'../components/common/components/waterfallsFlow'),
-    AEleven:path.resolve(__dirname,'../components/fantastic/a11'),
-    Charts:path.resolve(__dirname,'../components/fantastic/charts')
+    Home: path.resolve(__dirname, '../components/home'),
+    VueStillLearn: path.resolve(__dirname, '../components/vue3_still_learn/vue-still-learn'),
+    NestedLines: path.resolve(__dirname, '../components/vue3_still_learn/nestedLines'),
+    ElementDemo: path.resolve(__dirname, '../components/element-demo'),
+    PixiDemo: path.resolve(__dirname, '../components/game/pixi-demo'),
+    ResponsiveImages: path.resolve(__dirname, '../components/responsiveImages/index'),
+    WaterfallsFlow: path.resolve(__dirname, '../components/common/components/waterfallsFlow'),
+    AEleven: path.resolve(__dirname, '../components/fantastic/a11'),
+    Charts: path.resolve(__dirname, '../components/fantastic/charts')
 };

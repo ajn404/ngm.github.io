08338c89b854fd35270f9beae7643d263dd78d01
ajn404 <nhy1423238345@163.com>
2021-11-28 02:55:52 +0800

    no message

--- docs/.vuepress/components/home.vue
+++ docs/.vuepress/components/home.vue
@@ -6,11 +6,29 @@
       <div class="calendar">ajn404</div>
       <Swiper
         class="swiper-container"
-        :effect="'fade'"
         :autoplay="{
-          delay: 2500,
+          delay: 3000,
           disableOnInteraction: false,
         }"
+        style="position: relative"
+        v-show="styleAnimate"
+      >
+        <SwiperSlide v-for="(i, index) in swiperSlideNums" :key="index" class=" animate__animated animate__zoomIn animate__infinite">
+          <el-image
+            :src="'https://picsum.photos/1080/480/?random=' + index"
+            fit="cover"
+            style="max-width: 100vw"
+            class="swiper-img"
+          ></el-image>
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+        class="swiper-container"
+        :autoplay="{
+          delay: 3000,
+          disableOnInteraction: false,
+        }"
+        v-show="!styleAnimate"
         style="position: relative"
       >
         <SwiperSlide v-for="(i, index) in swiperSlideNums" :key="index">
@@ -18,9 +36,16 @@
             :src="'https://picsum.photos/1080/480/?random=' + index"
             fit="cover"
             style="max-width: 100vw"
+            class="swiper-img"
           ></el-image>
         </SwiperSlide>
       </Swiper>
+      <el-button 
+      @click="resetSwiperAnimate"
+      
+      >
+      轮播动画
+      </el-button>
       <h3>使用wasd上下左右,聚焦后键入m关闭音乐r重制场景</h3>
       <div class="iframe-box">
         <iframe
@@ -72,13 +97,16 @@
         >
         </iframe>
       </div>
-      <div class="nice-u">
-        <h3>gitpage学习部署的</h3>
-        <h3>
-          <a href="https://ajn404.github.io/viteV3/"
-            >https://ajn404.github.io/viteV3/</a
-          >
-        </h3>
+      <h3>学习react gsap系列</h3>
+      <div class="iframe-box">
+        <iframe
+          style="width: 100%"
+          scrolling="yes"
+          src="https://ajn404.github.io/gsap_ex/"
+          frameborder="no"
+          allowfullscreen="true"
+        >
+        </iframe>
       </div>
     </div>
   </div>
@@ -95,14 +123,16 @@ import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/bundle";
 import "./common/styles/index.scss";
+import "animate.css";
 
 export default {
   data() {
     return {
       text: "hello editor",
-      swiperSlideNums: new Array(10),
+      swiperSlideNums: new Array(20),
       typeThing: "",
       typingStop: false,
+      styleAnimate:true,
     };
   },
   mounted() {
@@ -119,9 +149,8 @@ export default {
   methods: {
     initType() {
       let typedString = [
-        "大梦一场的 狗粮长先生",
         "四体不勤 五谷不分",
-        "前已无通路 后不见归途",
+        "文不能作词 武不能防身",
       ];
       let homeTypedFont = new Typed(".element", {
         strings: typedString,
@@ -150,31 +179,35 @@ export default {
       // });
 
       const height = document.body.scrollHeight;
-      let i = 10;
+      let i = 100;
       var time = setInterval(function () {
         window.scrollTo({
-          top:i,
-          behavior:'smooth'
+          top: i,
+          behavior: "smooth",
         });
-        i += 10;
+        i += 100;
         if (i >= height) {
           clearInterval(time);
           let j = height;
           setTimeout(() => {
             var toTop = setInterval(function () {
               window.scrollTo({
-                top:j,
-                behavior:'smooth'
+                top: j,
+                behavior: "smooth",
               });
               j -= 30;
               if (j < 0) {
                 clearInterval(toTop);
               }
             });
-          }, 1000);
+          }, 600);
         }
       }, 50);
     },
+
+    resetSwiperAnimate(){
+      this.styleAnimate =!this.styleAnimate
+    }
   },
   components: {
     // cssTransitionAndAnimation,

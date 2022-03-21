f0c33964fa093df0a3f31756276828e1fa25604c
ajn404 <nhy1423238345@163.com>
2022-03-21 16:19:05 +0800

    no message

---
+++ docs/.vuepress/components/fantastic/coding_chanlenges/primeSpiral.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+import P5 from "../../../resource/p5";
+export default {
+  name: "primeSpiral",
+  mounted() {
+    this.$nextTick(function (){
+      let judgeCanvasExist = document.getElementById("defaultCanvas0");
+      if (judgeCanvasExist === null) {
+        new P5(this.main, "box");
+      }
+    })
+
+  },
+  methods:{
+    main(_p5){
+      let _ = _p5;
+      console.log(p5)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
